4b1836db414b1bab05c3da91c3500010644f81ae
eyi1 <ej7729@gmail.com>
2018-03-28 16:17:42 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -9,8 +9,8 @@ yield (array[i])
 end
 
 # call your method here!
-hello_t(array) do |name|
-    if name.start_with? ("T")
-      puts "Hi, #{name}"
-    end
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
   end
+end
